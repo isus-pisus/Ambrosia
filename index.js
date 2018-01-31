be948ecdaf9aef02440075fcc294dc9ce90398b7
1-isus <think.believe.cr8@gmail.com>
2018-01-31 08:42:28 -0500

    moved interfacing w/ hardware and sensor reading to libs folder

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 require('./config/dev');
-var morgan  = require('morgan');
-var express = require('express');
+import { automateDevice } from './libs/control.lib.js';
+import { getTemperature, getHumidity } from './libs/readSensor.lib.js';
+
+const morgan  = require('morgan');
+const express = require('express');
 const passport = require('passport');
-const Data_point = require('./models/data'); // schema for chart data point
-const User = require('./models/user'); // schema for user info
+const Data_point = require('./models/data');
+const User = require('./models/user');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-var GPIO = require('onoff').Gpio;
-var http = require('http');
-var url = require('url');
-var dateFormat = require('dateformat');
-var unix = require('to-unix-timestamp');
-var shortid = require('shortid');
+const http = require('http');
+const url = require('url');
+const dateFormat = require('dateformat');
+const unix = require('to-unix-timestamp');
 var app = express();
 
  // sensors
-var sensorLib = require('node-dht-sensor');
-var ds18b20 = require('ds18b20');
-var rpiDhtSensor = require('rpi-dht-sensor');
 
 
-var io = require('socket.io')();
+const io = require('socket.io')();
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 mongoose.connect(process.env.DATABASE);
 
-// Initialize passport for use
 app.use(passport.initialize());
 
-// Bring in defined Passport Strategy
 require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,14 +34,14 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use( (req, res, next) => {
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
 });
 app.use(morgan('dev'));
 
-app.post('/register', function(req, res) {
+app.post('/register', (req, res) => {
   if(!req.body.login || !req.body.email || !req.body.password) {
     res.status(400).json({ success: false, message: 'Please enter email and password.' });
   } else {
@@ -56,7 +52,7 @@ app.post('/register', function(req, res) {
     });
 
     // Attempt to save the user
-    newUser.save(function(err) {
+    newUser.save( (err) => {
       if (err) {
         return res.status(400).json({ success: false, message: 'That email address already exists.'});
       }
@@ -65,8 +61,7 @@ app.post('/register', function(req, res) {
   }
 });
 
-// Authenticate the user and get a JSON Web Token to include in the header of future requests.
-app.post('/login', function(req, res) {
+app.post('/login', (req, res) => {
   User.findOne({
     email: req.body.email
   }, function(err, user) {
@@ -76,13 +71,10 @@ app.post('/login', function(req, res) {
       res.status(401).json({ success: false, message: 'Authentication failed. User not found.' });
     } else {
       // Check if password matches
-      user.comparePassword(req.body.password, function(err, isMatch) {
+      user.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch && !err) {
-          // Create token if the password matched and no error was thrown
           const token = jwt.sign(user, process.env.HASH_SECRET, {
-            expiresIn: 10080 // in seconds
           });
-          // console.log(token);
           res.status(200).json({ success: true, token: 'JWT ' + token });
         } else {
           res.status(401).json({ success: false, message: 'Authentication failed. Passwords did not match.' });
@@ -94,17 +86,17 @@ app.post('/login', function(req, res) {
 
 // find user and attemp authentication, ruturn status and token if Authenticated
 
-app.post('/token', function(req, res) {
+app.post('/token', (req, res) => {
   console.log(req.body.password, req.body.email);
   User.findOne({
     email: req.body.email
-  }, function(err, user) {
+  }, (err, user) => {
     if (err) throw err;
 
     if (!user) {
       res.status(401).json({ success: false, message: 'Authentication failed. User not found.' });
     } else {
-      user.comparePassword(req.body.password, function(err, isMatch) {
+      user.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch && !err) {
           const token = jwt.sign(user, process.env.HASH_SECRET, {
             expiresIn: 10080 // in seconds
@@ -118,8 +110,8 @@ app.post('/token', function(req, res) {
   });
 });
 
-app.get('/points', function (req, res){
-  Data_point.find({}, function(err, points){
+app.get('/points', (req, res) => {
+  Data_point.find({}, (err, points) => {
     if (err) {
       console.log('an error occured');
     } else {
@@ -128,15 +120,12 @@ app.get('/points', function (req, res){
   })
 });
 
-app.get('/points/:date', function (req, res){
-  // console.log(req.params);
-  Data_point.find({date: req.params.date}, function(err, points){
+app.get('/points/:date', (req, res) => {
+  Data_point.find({date: req.params.date}, (err, points) => {
     if (err) {
       console.log('an error occured');
-    // } if(!points.data){
     }
     if(points.length == 0){
-      // console.log(points);
       res.status(200).json({ success: false, msg: "No data found for "+req.params.date});
     } else {
       console.log(points);
@@ -145,8 +134,8 @@ app.get('/points/:date', function (req, res){
   })
 });
 
-app.delete('/points/:date', requireAuth, function (req, res){
-  Data_point.remove({date: req.params.date},false, function(err, points){
+app.delete('/points/:date', requireAuth, (req, res) => {
+  Data_point.remove({date: req.params.date},false, (err, points) => {
     if (err) {
       console.log('an error occured');
     } else {
@@ -156,58 +145,44 @@ app.delete('/points/:date', requireAuth, function (req, res){
   });
 });
 
-//adding pump functionality
-
-var readout = sensorLib.read(11, 6);
-var led = new GPIO(17, 'out');
-
-io.on('connection', function(socket){
-  var now = new Date();
-  var emit_data = {
+io.on('connection', (socket) => {
+  let now = new Date();
+  let emit_data = {
     time: dateFormat(now, "h:MM TT"),
     date: dateFormat(now, "mmmm d, yyyy"),
     point: {
-      temp: ds18b20.temperatureSync('28-00000853833b'),
-      humidity: readout.humidity.toFixed(0)
+      temp: getTemperature(),
+      humidity: getHumidity()
     }
   };
 
   io.emit('justConnected', emit_data);
-  socket.on('pump', function(params){
-    if (params.state == true){
-      led.writeSync(1);
-    }else{
-      led.writeSync(0);
-    }
+  socket.on('automate', (params) => {
+    automateDevice(params.device, params.state, params.duration);
   });
 });
 
-
-setInterval( function (){
-  var now = new Date();
-
-  var data = {
-    timeStamp: dateFormat(now, "h:MM TT"),
-    point: {
-      temp: ds18b20.temperatureSync('28-00000853833b'),
-      humidity: readout.humidity.toFixed(0)
-    }
-  };
-  var emit_data = {
+setInterval( ()=> {
+  let now = new Date();
+  let emit_data = {
     time: dateFormat(now, "h:MM TT"),
     date: dateFormat(now, "mmmm d, yyyy"),
     point: {
-      temp: ds18b20.temperatureSync('28-00000853833b'),
-      humidity: readout.humidity.toFixed(0)
+      temp: getTemperature(),
+      humidity: getHumidity()
     }
   };
+
   io.emit('temperature', emit_data);
+
   const newPoint = new Data_point({
-    _id: shortid.generate(),
-    createdAt: unix(new Date()),
-    date: dateFormat(now, "mmmm d, yyyy"),
-    temp: ds18b20.temperatureSync('28-00000853833b'),
-    humidity: readout.humidity.toFixed(0)
+    temp: getTemperature(),
+    humidity: getHumidity(),
+    lightSensor: 0,
+    turbidity: 0,
+    nitrogen: 0,
+    co2: 0,
+    pH: 0
   });
 
   newPoint.save(function(err) {
@@ -222,22 +197,22 @@ setInterval( function (){
 	collecting and sending data from pH, Nitrogen, co2, Turbidity and light sensor every two minute
  **/
 
-setInterval( function() {
-  var variousSensorData  = {
+setInterval( () => {
+  let emit_data  = {
   	lightSensor: 0,
-  	preTurbidity: 0,
-  	postTurbidity: 0,
+  	turbidity: 0,
   	nitrogen: 0,
   	co2: 0,
   	pH: 0
   };
-  io.emit('liveStreamData', variousSensorData);
+  io.emit('liveStreamData', emit_data);
+
+  // TODO: add sonctionality to save sensor data to database
 
 }, 1*60*1000);
 
-
 io.listen(process.env.SOCKET_PORT);
-app.listen(process.env.SERVER_PORT, function(){
+app.listen(process.env.SERVER_PORT, () => {
     console.log('running on local host 3000');
 });
 
